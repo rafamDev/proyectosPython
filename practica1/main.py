5ff3b4cfd0b876d94f77f625f1a72c0e9aecfee4
RafaelMayo <rafamayo7@gmail.com>
2021-02-02 21:49:16 +0100

    actualizado

--- practica1/main.py
+++ practica1/main.py
@@ -1,15 +1,37 @@
-#Algoritmo que pida tres números y los muestre ordenados (de mayor a menor).
+#Algoritmo que pida tres números, obten la media muestralos ordenados (de mayor a menor).
+
 
 def pedirNumero():
     return int (input("Introduce Numero: "))
 
-def ordenarNumeros():
-    numeros = [pedirNumero(),pedirNumero(),pedirNumero()]
-    numeros.sort()
-    print("Numeros ordenados de menor a meyor: ",numeros)
-    numeros.sort(reverse=True)
-    print("Numeros ordenados de mayor a menor: ",numeros)
+def calculo():
+    try:
+        numeros = [pedirNumero(), pedirNumero(), pedirNumero()]
+        mayorMenor(numeros)
+        acumulador = 0
+        contador = 0
+        for numero in numeros:
+            acumulador = acumulador + numero
+            contador = contador + 1
+        print("La media es: ", (acumulador / contador))
+
+    except ZeroDivisionError:
+         print("División por cero!")
+
+
+def mayorMenor(numeros):
+    for numero in numeros:
+        if (numero < numeros[0]):
+            print("Numero menor: ", numero)
+        if (numero > numeros[0]):
+            print("Numero mayor: ", numero)
+
 
 
 if __name__ == '__main__':
-    ordenarNumeros()
+    while True:
+        try:
+            calculo()
+            break
+        except ValueError:
+            print("¡Debe introducir NUMEROS!")
