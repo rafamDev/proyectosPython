8c957b6927169eee3e5de42e3e0d858daf9212be
rafamDev <76625223+rafamDev@users.noreply.github.com>
2021-02-02 23:04:56 +0100

    actualizado

--- entrada_salida/main.py
+++ entrada_salida/main.py
@@ -1,14 +1,14 @@
 
-def entrada():
+def datos():
     nombreProducto = input("Introduce nombre producto: ")
     cantidad = int (input("Introduce cantidad: "))
     precio = float (input("Introduce precio: "))
     salida(nombreProducto,cantidad,precio)
 
-def salida(nombreProducto,cantidad,precio):
+def mostrarDatos(nombreProducto,cantidad,precio):
     print("El nombre del producto es:",nombreProducto)
     print("Cantidad:",cantidad,", ",end="") #end="" evita un salto de linea. // #Otra forma: print("Cantidad: ",str(cantidad))
     print("Precio: %.2f"%(precio))
 
 if __name__ == '__main__':
-   entrada()
+   datos()
