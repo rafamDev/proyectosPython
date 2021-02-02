b04f2c19d5f2e3420df7372ebbcabba3db79fdc5
rafamDev <76625223+rafamDev@users.noreply.github.com>
2021-02-02 23:03:56 +0100

    actualizado

--- entrada_salida/main.py
+++ entrada_salida/main.py
@@ -7,11 +7,8 @@ def entrada():
 
 def salida(nombreProducto,cantidad,precio):
     print("El nombre del producto es:",nombreProducto)
-    print("Cantidad:",cantidad,", ",end="") #end="" evita un salto de linea. #print("Cantidad: ",str(cantidad))
+    print("Cantidad:",cantidad,", ",end="") #end="" evita un salto de linea. // #Otra forma: print("Cantidad: ",str(cantidad))
     print("Precio: %.2f"%(precio))
 
-def funcion():
-    entrada()
-
 if __name__ == '__main__':
-   funcion()
+   entrada()
