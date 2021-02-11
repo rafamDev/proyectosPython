c66b2bfd627f762f03b1eb3e6c3c9ace2ab40257
RafaelMayo <rafamayo7@gmail.com>
2021-02-11 10:31:54 +0100

    finished

--- Ej1RRMFG/principal.py
+++ Ej1RRMFG/principal.py
@@ -3,72 +3,68 @@ import random
 def generarNumeroAleatorio(min,max):
     return random.randint(min, max)
 
-def puntuacionBanca():
-    acumulador = 0
-    contador = 0
+def generarPartida():
+    puntos = 0
+    intentos = 0
     while True:
         numero = generarNumeroAleatorio(1,11)
-        acumulador = acumulador + numero
-        contador = contador + 1
-        if acumulador > 15:
+        puntos = puntos + numero
+        intentos = intentos + 1
+        if puntos > 15:
             break
-    return acumulador
+    return puntos, intentos
 
+def final(croupierPuntos,jugadorPuntos,jugadorIntentos,croupierIntentos):
+    if jugadorPuntos > croupierPuntos:
+        print("(Jugador) Has Ganado!!")
+    if jugadorPuntos < croupierPuntos:
+        print("(Banca) ha ganado!!")
+    if jugadorPuntos == croupierPuntos:
+        if jugadorIntentos > croupierIntentos:
+             print("(Banca) ha perdido!!")
+        if jugadorIntentos < croupierIntentos:
+            print("(Jugador) Has perdido!!")
 
-def subMenu():
+def subMenu(croupierPuntos,jugadorPuntos,jugadorIntentos,croupierIntentos):
     print("\n¿Seguir Jugando?")
     print("1-Si\n2-No\n")
-    opcion = int(input("Introduzca opcion: "))
+    opcion = int(input("Introduzca opcion: \n"))
     if opcion == 1:
-        dificultad("normal")
+        menu()
+    if opcion == 2:
+        final(croupierPuntos,jugadorPuntos,jugadorIntentos,croupierIntentos)
 
+def estadoPartida(jugadorPuntos,croupierPuntos,jugadorIntentos,croupierIntentos):
+    print("Puntuacion del jugador: ", jugadorPuntos)
+    if jugadorPuntos > 21:
+        print("(Jugador) Has perdido!!")
+    if jugadorPuntos == 21:
+        print("(Croupier) ha perdido!!")
+    if jugadorPuntos < 21:
+        subMenu(croupierPuntos,jugadorPuntos,jugadorIntentos,croupierIntentos)
 
-def jugador(puntuacion):
-    return puntuacion
-
-
-def banca(puntuacion):
-    return puntuacion
-
-
-def dificultad(nivel):
-   puntuacion = puntuacionBanca()
-   if nivel == "facil":
-       print("Puntuacion del jugador: ", puntuacion)
-   print("Numero aleatorio: ", generarNumeroAleatorio(1, 11))
-   if puntuacion > 21:
-       print("(Jugador) Has perdido!!")
-       jugador(puntuacion)
-   if puntuacion == 21:
-       print("(Banca) ha perdido!!")
-       banca(puntuacion)
-   if puntuacion < 21:
-       subMenu()
-
-
-
-
-def normal():
-    pass
-
+def dificultad(dificil):
+   jugadorPuntos,jugadorIntentos = generarPartida()
+   croupierPuntos,croupierIntentos = generarPartida()
+   if dificil == False:
+      print("Puntuacion del croupier: ", croupierPuntos)
+   estadoPartida(jugadorPuntos,croupierPuntos,jugadorIntentos,croupierIntentos)
 
 def menu():
   print("\n---BLACKJACK---")
   while True:
    try:
      print("\n1-Modo Facil\n2-Modo Normal\n0-Salir\n")
-     opcion = int(input("Introduzca opcion: "))
+     opcion = int(input("Introduzca opcion: \n"))
      if opcion == 1:
-        dificultad("facil")
+        dificultad(False)
      if opcion == 2:
-        normal()    
+        dificultad(True)
      if opcion == 0:
         print("--PROGRAMA FINALIZADO--")
         exit(0)
    except ValueError:
        print("Inserte uno de los numeros del menu")
-
-
 
 if __name__ == '__main__':
     menu()
